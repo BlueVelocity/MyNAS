502a9ca03ae3c038c7b0dd247bcc4e6cca42196d
BlueVelocity <josephrodney@hotmail.com>
2025-01-19 01:33:20 -0500

    Simplify file structure
    For single page app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,16 @@ import express from "express";
 const server = express();
 const port = 5000;
 
-import loginPage from "./routes/login.js";
-
 server.use(express.static("/frontend/dist/"))
 
-server.use('/', loginPage);
+const log = (req: any, res: any, next: any) => {
+  console.log('Please login', Date.now())
+  next()
+}
+
+server.get('/', (req: any, res: any) => {
+  res.sendFile("index.html")
+})
 
 server.listen(port, () => {
   console.log(`Example server listening on port ${port}`);
